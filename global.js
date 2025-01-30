1b835dc6b8b3c12734fdb6cc0d21795c994122c5
stephanieyyue <63174977+stephanieyyue@users.noreply.github.com>
2025-01-30 15:48:36 -0800

    async function

--- global.js
+++ global.js
@@ -80,3 +80,27 @@ form?.addEventListener('submit', (event) => {
   
   location.href = url.slice(0, -1); // Remove trailing &
 });
+
+export async function fetchJSON(url) {
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+          throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  const article = document.createElement('article');
+  article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+  `;
+  containerElement.appendChild(article);
+}
