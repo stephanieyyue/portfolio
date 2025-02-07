48d5f20ba67a204f4a777e88b4fc2cd2295b38ca
stephanieyyue <63174977+stephanieyyue@users.noreply.github.com>
2025-02-07 15:53:25 -0800

    d3 fix

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,5 @@
 import { fetchJSON } from '../global.js';
-import * as d3 from 'https://unpkg.com/d3@7?module';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 console.log("projects.js is running!");
 
