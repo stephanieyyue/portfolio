629e8290858f16fbf4caa89ebb19ea9cd4a9b5c7
stephanieyyue <63174977+stephanieyyue@users.noreply.github.com>
2025-02-07 10:03:52 -0800

    circle

--- projects/projects.js
+++ projects/projects.js
@@ -2,27 +2,49 @@ import { fetchJSON } from '../global.js';
 
 console.log("projects.js is running!");
 
-function createVisualization(container) {
+function addSvg(container) {
+    // Create SVG container before first child
     const svg = d3.select(container)
         .insert('svg', ':first-child')
-        .attr('viewBox', '0 0 100 100')
-        .style('width', '20em')
-        .style('margin-block', '2em')
-        .style('display', 'block');
+        .attr('width', '100')
+        .attr('height', '100')
+        .style('margin', '2em 0');
 
+    // Add circle
     svg.append('circle')
-        .attr('cx', 50)
-        .attr('cy', 50)
-        .attr('r', 50)
+        .attr('cx', '50')
+        .attr('cy', '50')
+        .attr('r', '50')
         .attr('fill', 'red');
 }
 
-function renderProjects(projects, container, headingLevel = 'h2') {
-    // Add visualization
-    createVisualization(container);
+async function init() {
+    try {
+        const projects = await fetchJSON('../lib/projects.json');
+        const projectsContainer = document.querySelector('.projects');
+        const projectsTitle = document.querySelector('.projects-title');
 
+        if (projectsTitle && projects) {
+            projectsTitle.textContent = `${projects.length} Projects`;
+        }
+
+        if (projectsContainer) {
+            projectsContainer.innerHTML = '';
+            addSvg(projectsContainer);
+        }
+
+        if (projectsContainer && projects) {
+            renderProjects(projects, projectsContainer, 'h2');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function renderProjects(projects, container, headingLevel = 'h2') {
     projects.forEach(project => {
         const article = document.createElement('article');
+        
         const heading = document.createElement(headingLevel);
         heading.textContent = project.title;
         
@@ -51,28 +73,6 @@ function renderProjects(projects, container, headingLevel = 'h2') {
         
         container.appendChild(article);
     });
-}
-
-async function init() {
-    try {
-        const projects = await fetchJSON('../lib/projects.json');
-        const projectsContainer = document.querySelector('.projects');
-        const projectsTitle = document.querySelector('.projects-title');
-
-        if (projectsTitle && projects) {
-            projectsTitle.textContent = `${projects.length} Projects`;
-        }
-
-        if (projectsContainer) {
-            projectsContainer.innerHTML = '';
-        }
-
-        if (projectsContainer && projects) {
-            renderProjects(projects, projectsContainer, 'h2');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
 }
 
 init();
