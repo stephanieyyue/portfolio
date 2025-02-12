9b92fe3c24512a44494ab517d162037c6f09909a
stephanieyyue <63174977+stephanieyyue@users.noreply.github.com>
2025-02-12 14:25:25 -0800

    console

--- meta/main.js
+++ meta/main.js
@@ -1,9 +1,16 @@
 let data = [];
 
 async function loadData() {
-    data = await d3.csv('loc.csv');
+    data = await d3.csv('loc.csv', (row) => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + 'T00:00' + row.timezone),
+      datetime: new Date(row.datetime),
+    }));
     console.log(data);
-}
+  }
 
 document.addEventListener('DOMContentLoaded', async () => {
     await loadData();
